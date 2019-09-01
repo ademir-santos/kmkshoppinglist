e20b9ded6251f2b2ad4556d300873335a14deff6
Ademir Aparecido dos Santos Oliveira <ademirap.santos@outlook.com>
2019-09-01 04:36:03 -0300

    add second url api
    Signed-off-by: Ademir Aparecido dos Santos Oliveira <ademirap.santos@outlook.com>

--- lib/json-class/variable-json.dart
+++ lib/json-class/variable-json.dart
@@ -5,49 +5,49 @@ String url(typeUrl, link) {
       if(link == 1)
         return "http://192.168.25.20:5000/api/Login";
       else
-        return "http://192.168.25.20:5000/api/Login";
+        return "http://kmkconsultoria.ddns.net:64020/api/Login";
       break;
     case "Users":
       if(link == 1)
         return "http://192.168.25.20:5000/api/Users/";
       else
-        return "192.168.25.20:5000/api/Users/";
+        return "http://kmkconsultoria.ddns.net:64020/api/Users/";
       break;
     case "Category":
       if(link == 1)
         return "http://192.168.25.20:5000/api/Category";
       else
-        return "http://192.168.25.20:5000/api/Category";
+        return "http://kmkconsultoria.ddns.net:64020/api/Category";
       break;
     case "Product":
       if(link == 1)
         return "http://192.168.25.20:5000/api/Product";
       else
-        return "http://192.168.25.20:5000/api/Product";
+        return "http://kmkconsultoria.ddns.net:64020/api/Product";
       break;
     case "TablePrice":
       if(link == 1)
         return "http://192.168.25.20:5000/api/TablePrice";
       else
-        return "http://192.168.25.20:5000/api/TablePrice";
+        return "http://kmkconsultoria.ddns.net:64020/api/TablePrice";
       break;
     case "UserShopplist":
       if(link == 1)
         return "1http://192.168.25.20:5000/api/UserShopplist";
       else
-        return "http://192.168.25.20:5000/api/UserShopplist";
+        return "http://kmkconsultoria.ddns.net:64020/api/UserShopplist";
       break;
     case "ShopplistCategory":
       if(link == 1)  
         return "http://192.168.25.20:5000/api/ShopplistCategory";
       else
-        return "http://192.168.25.20:5000/api/ShopplistCategory";
+        return "http://kmkconsultoria.ddns.net:64020/api/ShopplistCategory";
       break;
     case "ShopplistCategoryProduct":
       if(link == 1)
         return "http://192.168.25.20:5000/api/ShopplistCategoryProduct";
       else
-        return "http://192.168.25.20:5000/api/ShopplistCategoryProduct";
+        return "http://kmkconsultoria.ddns.net:64020/api/ShopplistCategoryProduct";
       break;
   }
 
